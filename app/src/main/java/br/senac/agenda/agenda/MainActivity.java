bb1465e1e9342daab1f1fafbeba01800840b2cc9
alysson.mgodoy <alysson651@gmail.com>
2019-02-12 22:16:55 -0200

    Commit inicial

--- app/src/main/java/br/senac/agenda/agenda/MainActivity.java
+++ app/src/main/java/br/senac/agenda/agenda/MainActivity.java
@@ -1,8 +1,11 @@
 package br.senac.agenda.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lista = findViewById(R.id.listaContatos);
+        ListView list = findViewById(R.id.listContactsListView);
 
-        String[] contatos = {"André", "Ari", "Roberto", "Luzia"};
+        String[] contacts = {"André", "Ari", "Roberto", "Luzia"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contatos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
 
-        lista.setAdapter(adapter);
+        list.setAdapter(adapter);
+
+        Button newContact = findViewById(R.id.newContactButton);
+
+        newContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contact = new Intent(MainActivity.this, ContatosActivity.class);
+                startActivity(contact);
+            }
+        });
     }
 }
